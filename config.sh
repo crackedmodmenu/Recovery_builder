e18f2233ccb1145933c67c06d2e744fe832616ba
crackedmodmenu <121314959+crackedmodmenu@users.noreply.github.com>
2023-06-20 08:39:59 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash env
 
 # about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export NAME="pbrp" # supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb" # the link of manifest
+export BRANCH="android-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="merlinx" # codename used in device tree
+export DT_LINK="https://github.com/crackedmodmenu/potato_tree_merlinx" # device tree link
+export DT_BRANCH="main" # device tree branch
+export VENDOR="xiaomi" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
